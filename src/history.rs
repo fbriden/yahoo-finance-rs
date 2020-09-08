9f34fc1f6cb0521674f04bb6d518a687e9b0bba1
Fred Briden <fred.briden@gmail.com>
2020-09-07 21:52:57 -0400

    23: More graceful failures (#24)
    When Yahoo sends us an empty dictionary of quote data we should
    not crash and we should pass that on as an empty array of results.

--- src/history.rs
+++ src/history.rs
@@ -4,28 +4,40 @@ use snafu::{ensure, OptionExt};
 use crate::{error, yahoo, Bar, Interval, Result};
 
 fn aggregate_bars(data: yahoo::Data) -> Result<Vec<Bar>> {
+   let mut result = Vec::new();
+
    let timestamps = &data.timestamps;
    let quotes = &data.indicators.quotes;
-   ensure!(!quotes.is_empty(), error::MissingData { reason: "missing quotes data" });
 
+   // if we have no timestamps & no quotes we'll assume there is no data
+   if timestamps.is_empty() && quotes.is_empty() { return Ok(result); }
+
+   // otherwise see if one is empty and reflects bad data from Yahoo!
+   ensure!(!timestamps.is_empty(), error::MissingData { reason: "no timestamps for OHLCV data" });
+   ensure!(!quotes.is_empty(), error::MissingData { reason: "no OHLCV data" });
+
+   // make sure timestamps lines up with the OHLCV data
    let quote = &quotes[0];
-   ensure!(timestamps.len() == quote.volume.len(), error::MissingData { reason: "dates do not line up with quotes" });
+   ensure!(timestamps.len() == quote.volumes.len(), error::MissingData { reason: "timestamps do not line up with OHLCV data" });
+   ensure!(timestamps.len() == quote.opens.len(), error::MissingData { reason: "'open' values do not line up the timestamps" });
+   ensure!(timestamps.len() == quote.highs.len(), error::MissingData { reason: "'high' values do not line up the timestamps" });
+   ensure!(timestamps.len() == quote.lows.len(), error::MissingData { reason: "'low' values do not line up the timestamps" });
+   ensure!(timestamps.len() == quote.closes.len(), error::MissingData { reason: "'close' values do not line up the timestamps" });
 
-   let mut result = Vec::new();
    #[allow(clippy::needless_range_loop)]
    for i in 0..timestamps.len() {
       // skip days where we have incomplete data
-      if quote.open[i].is_none() || quote.high[i].is_none() || quote.low[i].is_none() || quote.close[i].is_none() {
+      if quote.opens[i].is_none() || quote.highs[i].is_none() || quote.lows[i].is_none() || quote.closes[i].is_none() {
          continue;
       }
 
       result.push(Bar {
          timestamp: timestamps[i] * 1000,
-         open: quote.open[i].context(error::InternalLogic{ reason: "missing open not caught" })?,
-         high: quote.high[i].context(error::InternalLogic{ reason: "missing high not caught" })?,
-         low: quote.low[i].context(error::InternalLogic{ reason: "missing low not caught" })?,
-         close: quote.close[i].context(error::InternalLogic{ reason: "missing close not caught" })?,
-         volume: quote.volume[i],
+         open: quote.opens[i].context(error::InternalLogic{ reason: "missing open not caught" })?,
+         high: quote.highs[i].context(error::InternalLogic{ reason: "missing high not caught" })?,
+         low: quote.lows[i].context(error::InternalLogic{ reason: "missing low not caught" })?,
+         close: quote.closes[i].context(error::InternalLogic{ reason: "missing close not caught" })?,
+         volume: quote.volumes[i],
       })
    }
    Ok(result)
@@ -38,12 +50,18 @@ fn aggregate_bars(data: yahoo::Data) -> Result<Vec<Bar>> {
 ///
 /// Get 6 months worth of Apple data:
 ///
-/// ``` no-run
+/// ``` no_run
 /// use yahoo_finance::{ history, Timestamped };
 ///
-/// let data = history::retrieve("AAPL").unwrap();
-/// for bar in &data {
-///    println!("On {} Apple closed at ${:.2}", bar.datetime().format("%b %e %Y"), bar.close)
+/// #[tokio::main]
+/// async fn main() {
+///    match history::retrieve("AAPL").await {
+///       Err(e) => println!("Failed to call Yahoo: {:?}", e),
+///       Ok(data) => 
+///          for bar in &data {
+///             println!("On {} Apple closed at ${:.2}", bar.datetime().format("%b %e %Y"), bar.close)
+///          }
+///    }
 /// }
 /// ```
 pub async fn retrieve(symbol: &str) -> Result<Vec<Bar>> {
@@ -59,12 +77,18 @@ pub async fn retrieve(symbol: &str) -> Result<Vec<Bar>> {
 ///
 /// Get 5 days worth of Apple data:
 ///
-/// ``` no-run
+/// ``` no_run
 /// use yahoo_finance::{ history, Interval, Timestamped };
 ///
-/// let data = history::retrieve_interval("AAPL", Interval::_5d).unwrap();
-/// for bar in &data {
-///    println!("On {} Apple closed at ${:.2}", bar.datetime().format("%b %e %Y"), bar.close)
+/// #[tokio::main]
+/// async fn main() {
+///    match history::retrieve_interval("AAPL", Interval::_5d).await {
+///       Err(e) => println!("Failed to call Yahoo: {:?}", e),
+///       Ok(data) => 
+///          for bar in &data {
+///             println!("On {} Apple closed at ${:.2}", bar.datetime().format("%b %e %Y"), bar.close)
+///          }
+///    }
 /// }
 /// ```
 pub async fn retrieve_interval(symbol: &str, interval: Interval) -> Result<Vec<Bar>> {
@@ -80,14 +104,20 @@ pub async fn retrieve_interval(symbol: &str, interval: Interval) -> Result<Vec<B
 ///
 /// Get 5 days worth of Apple data:
 ///
-/// ``` no-run
+/// ``` no_run
 /// use chrono::{Duration, TimeZone, Utc};
 /// use yahoo_finance::{ history, Timestamped };
 ///
-/// let now = Utc::now();
-/// let data = history::retrieve_range("AAPL", now - Duration::days(30), Some(now - Duration::days(10))).unwrap();
-/// for bar in &data {
-///    println!("On {} Apple closed at ${:.2}", bar.datetime().format("%b %e %Y"), bar.close)
+/// #[tokio::main]
+/// async fn main() {
+///    let now = Utc::now();
+///    match history::retrieve_range("AAPL", now - Duration::days(30), Some(now - Duration::days(10))).await {
+///       Err(e) => println!("Failed to call Yahoo {:?}", e),
+///       Ok(data) =>
+///          for bar in &data {
+///             println!("On {} Apple closed at ${:.2}", bar.datetime().format("%b %e %Y"), bar.close)
+///          }
+///    }
 /// }
 /// ```
 pub async fn retrieve_range(symbol: &str, start: DateTime<Utc>, end: Option<DateTime<Utc>>) -> Result<Vec<Bar>> {
