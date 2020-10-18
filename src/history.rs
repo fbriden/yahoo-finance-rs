99691b577a1fe5f1c9348ffe416a68c6c5b89327
Patrick Fernie <patrick.fernie@gmail.com>
2020-10-18 15:21:39 -0400

    Add support for corporate action events
    * Adds `Dividend` and `Split` types
    * Adds `*_with_events()` versions of functions in the `history` API,
    which return, along with `Bar` data, any `Dividend` or `Split` actions
    in the applicable period.

--- src/history.rs
+++ src/history.rs
@@ -3,14 +3,27 @@ use snafu::{ensure, OptionExt};
 
 use crate::{error, yahoo, Bar, Interval, Result};
 
-fn aggregate_bars(data: yahoo::Data) -> Result<Vec<Bar>> {
-   let mut result = Vec::new();
+fn extract_events(data: yahoo::Data) -> (Vec<yahoo::Dividend>, Vec<yahoo::Split>) {
+    let events = match data.events {
+        None => return (Vec::new(), Vec::new()),
+        Some(events) => events,
+    };
+    // The API returns events as a map by date; here we simply flatten to a `Vec`
+    let dividends = events.dividends.map(|ds| ds.into_iter().map(|(_date, mut dividend)| { dividend.timestamp *= 1000; dividend }).collect()).unwrap_or_else(Vec::new);
+    let splits = events.splits.map(|ss| ss.into_iter().map(|(_date, mut split)| { split.timestamp *= 1000; split }).collect()).unwrap_or_else(Vec::new);
+    (dividends, splits)
+}
 
+fn aggregate_bars_and_extract_events(data: yahoo::Data) -> Result<(Vec<Bar>, Vec<yahoo::Dividend>, Vec<yahoo::Split>)> {
+   let mut result = Vec::new();
    let timestamps = &data.timestamps;
    let quotes = &data.indicators.quotes;
 
    // if we have no timestamps & no quotes we'll assume there is no data
-   if timestamps.is_empty() && quotes.is_empty() { return Ok(result); }
+   if timestamps.is_empty() && quotes.is_empty() {
+       let (dividends, splits) = extract_events(data);
+       return Ok((result, dividends, splits));
+   }
 
    // otherwise see if one is empty and reflects bad data from Yahoo!
    ensure!(!timestamps.is_empty(), error::MissingData { reason: "no timestamps for OHLCV data" });
@@ -40,7 +53,9 @@ fn aggregate_bars(data: yahoo::Data) -> Result<Vec<Bar>> {
          volume: quote.volumes[i],
       })
    }
-   Ok(result)
+
+   let (dividends, splits) = extract_events(data);
+   Ok((result, dividends, splits))
 }
 
 /// Retrieves (at most) 6 months worth of OCLHV data for a symbol
@@ -65,7 +80,38 @@ fn aggregate_bars(data: yahoo::Data) -> Result<Vec<Bar>> {
 /// }
 /// ```
 pub async fn retrieve(symbol: &str) -> Result<Vec<Bar>> {
-   aggregate_bars(yahoo::load_daily(symbol, Interval::_6mo).await?)
+   aggregate_bars_and_extract_events(yahoo::load_daily(symbol, Interval::_6mo).await?).map(|(bars, _dividends, _splits)| bars)
+}
+
+/// Retrieves (at most) 6 months worth of OCLHV and corporate action event data for a symbol
+///
+/// # Examples
+///
+/// Get any Apple corporate actions within the last 6 months:
+///
+/// ``` no_run
+/// use yahoo_finance::{ history, Timestamped };
+///
+/// #[tokio::main]
+/// async fn main() {
+///    match history::retrieve_with_events("AAPL").await {
+///       Err(e) => println!("Failed to call Yahoo: {:?}", e),
+///       Ok((bars, dividends, splits)) => {
+///          for bar in &bars {
+///             println!("On {} Apple closed at ${:.2}", bar.datetime().format("%b %e %Y"), bar.close)
+///          }
+///          for dividend in &dividends {
+///             println!("Apple dividend of {} on {}", dividend.amount, dividend.datetime().format("%b %e %Y"))
+///          }
+///          for split in &splits {
+///             println!("Apple split {}:{} on {}", split.numerator, split.denominator, split.datetime().format("%b %e %Y"))
+///          }
+///       }
+///    }
+/// }
+/// ```
+pub async fn retrieve_with_events(symbol: &str) -> Result<(Vec<Bar>, Vec<yahoo::Dividend>, Vec<yahoo::Split>)> {
+    yahoo::load_daily_with_events(symbol, Interval::_6mo).await.and_then(aggregate_bars_and_extract_events)
 }
 
 /// Retrieves a configurable amount of OCLHV data for a symbol
@@ -95,7 +141,42 @@ pub async fn retrieve_interval(symbol: &str, interval: Interval) -> Result<Vec<B
    // pre-conditions
    ensure!(!interval.is_intraday(), error::NoIntraday { interval });
 
-   aggregate_bars(yahoo::load_daily(symbol, interval).await?)
+   aggregate_bars_and_extract_events(yahoo::load_daily(symbol, interval).await?).map(|(bars, _dividends, _splits)| bars)
+}
+
+/// Retrieves a configurable amount of OCLHV and corporate action event data for a symbol.  The amount of
+/// data returned might be less than the interval specified if the symbol is new.
+///
+/// # Examples
+///
+/// Get 5 days worth of Apple data:
+///
+/// ``` no_run
+/// use yahoo_finance::{ history, Interval, Timestamped };
+///
+/// #[tokio::main]
+/// async fn main() {
+///    match history::retrieve_interval_with_events("AAPL", Interval::_5d).await {
+///       Err(e) => println!("Failed to call Yahoo: {:?}", e),
+///       Ok((bars, dividends, splits)) => {
+///          for bar in &bars {
+///             println!("On {} Apple closed at ${:.2}", bar.datetime().format("%b %e %Y"), bar.close)
+///          }
+///          for dividend in &dividends {
+///             println!("Apple dividend of {} on {}", dividend.amount, dividend.datetime().format("%b %e %Y"))
+///          }
+///          for split in &splits {
+///             println!("Apple split {}:{} on {}", split.numerator, split.denominator, split.datetime().format("%b %e %Y"))
+///          }
+///       }
+///    }
+/// }
+/// ```
+pub async fn retrieve_interval_with_events(symbol: &str, interval: Interval) -> Result<(Vec<Bar>, Vec<yahoo::Dividend>, Vec<yahoo::Split>)> {
+   // pre-conditions
+   ensure!(!interval.is_intraday(), error::NoIntraday { interval });
+
+   yahoo::load_daily_with_events(symbol, interval).await.and_then(aggregate_bars_and_extract_events)
 }
 
 /// Retrieves OCLHV data for a symbol between a start and end date.
@@ -125,5 +206,42 @@ pub async fn retrieve_range(symbol: &str, start: DateTime<Utc>, end: Option<Date
    let _end = end.unwrap_or_else(Utc::now);
    ensure!(_end.signed_duration_since(start).num_seconds() > 0, error::InvalidStartDate);
 
-   aggregate_bars(yahoo::load_daily_range(symbol, start.timestamp(), _end.timestamp()).await?)
+   aggregate_bars_and_extract_events(yahoo::load_daily_range(symbol, start.timestamp(), _end.timestamp()).await?).map(|(bars, _dividends, _splits)| bars)
+}
+
+/// Retrieves OCLHV and corporate action event data for a symbol between a start and end date.
+///
+/// # Examples
+///
+/// Get 5 days worth of Apple data:
+///
+/// ``` no_run
+/// use chrono::{Duration, TimeZone, Utc};
+/// use yahoo_finance::{ history, Timestamped };
+///
+/// #[tokio::main]
+/// async fn main() {
+///    let now = Utc::now();
+///    match history::retrieve_range_with_events("AAPL", now - Duration::days(30), Some(now - Duration::days(10))).await {
+///       Err(e) => println!("Failed to call Yahoo {:?}", e),
+///       Ok((bars, dividends, splits)) => {
+///          for bar in &bars {
+///             println!("On {} Apple closed at ${:.2}", bar.datetime().format("%b %e %Y"), bar.close)
+///          }
+///          for dividend in &dividends {
+///             println!("Apple dividend of {} on {}", dividend.amount, dividend.datetime().format("%b %e %Y"))
+///          }
+///          for split in &splits {
+///             println!("Apple split {}:{} on {}", split.numerator, split.denominator, split.datetime().format("%b %e %Y"))
+///          }
+///       }
+///    }
+/// }
+/// ```
+pub async fn retrieve_range_with_events(symbol: &str, start: DateTime<Utc>, end: Option<DateTime<Utc>>) -> Result<(Vec<Bar>, Vec<yahoo::Dividend>, Vec<yahoo::Split>)> {
+   // pre-conditions
+   let _end = end.unwrap_or_else(Utc::now);
+   ensure!(_end.signed_duration_since(start).num_seconds() > 0, error::InvalidStartDate);
+
+   yahoo::load_daily_range_with_events(symbol, start.timestamp(), _end.timestamp()).await.and_then(aggregate_bars_and_extract_events)
 }
